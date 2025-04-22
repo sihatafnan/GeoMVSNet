44146efd5b1f486dcc43f886de9390300c3a5710
Sihat Afnan <57033114+sihatafnan@users.noreply.github.com>
2025-04-22 00:48:59 -0700

    Update data_path.sh

--- scripts/data_path.sh
+++ scripts/data_path.sh
@@ -6,7 +6,7 @@ DTU_TEST_ROOT="[/path/to/]dtu-test"
 DTU_QUANTITATIVE_ROOT="[/path/to/]dtu-evaluation"
 
 # Tanks and Temples
-TNT_ROOT="[/path/to/]tnt"
+TNT_ROOT="/content/drive/MyDrive/MVS/tankandtemples"
 
 # BlendedMVS
 BLENDEDMVS_ROOT="[/path/to/]blendmvs"
